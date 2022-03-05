e3db7b8413eec35c3e69ab40bdd0215288297db9
SanakulovDev <emailkerak@gmail.com>
2022-03-05 22:31:29 +0500

    first

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php 
-echo 123;
- ?>
+include 'Telegram.php';
+$telegram = new Telegram('1718864914:AAGdgFusZOVAi22wpnJO2OnUExyH5Tw-PUg');
+$chat_id = 1359920466;
+$text = $telegram->Text();
+if($text == "/start"){
+    $content = array('chat_id' => $chat_id, 'text'=>"Xush kelibsiz bizning botga");
+    $telegram->sendMessage($content);
+}
+
+?>
